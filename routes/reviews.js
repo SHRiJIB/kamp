41f2db68be16399151bc98deab1d4693ec2635cc
Shreejeeb <shreejeebkesh@gmail.com>
2021-06-05 16:50:16 +0530

    Authorization middleware

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,22 +1,13 @@
 const express = require('express')
-const { reviewSchema } = require('../schemas.js')
 const catchAsyncError = require('../utils/catchAsyncError.js')
-const ExpressError = require('../utils/ExpressError.js')
 const { addReview, deleteReview } = require('../controllers/reviews.js')
+const { isLoggedIn } = require('../middleware/authMw.js')
+const { validateReview } = require('../middleware/reviewMw.js')
 
 const router = express.Router({ mergeParams: true })
-const validateReview = (req, res, next) => {
-  const { error } = reviewSchema.validate(req.body)
-  if (error) {
-    const msg = error.details.map((ob) => ob.message).join(',')
-    throw new ExpressError(msg, 400)
-  } else {
-    next()
-  }
-}
 
 // add reviews to a campground
-router.post('/', validateReview, catchAsyncError(addReview))
+router.post('/', isLoggedIn, validateReview, catchAsyncError(addReview))
 
 //delete reviews
 router.delete('/:reviewId', catchAsyncError(deleteReview))
