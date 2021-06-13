c836aabb5163257f40b69b6faf3652b0848fa2c1
Shreejeeb <shreejeebkesh@gmail.com>
2021-06-13 15:27:34 +0530

    mapbox setup,map for every campground and cluster map at index page

--- seeds/index.js
+++ seeds/index.js
@@ -21,16 +21,26 @@ const titleGenerator = (array) => {
   return array[randInd]
 }
 
+const mbxToken = process.env.MAPBOX
+
 //60b771403c217b4a08a84ebf
 const seedDB = async () => {
   await Campground.deleteMany({})
 
-  for (let i = 0; i < 50; i++) {
+  for (let i = 0; i < 300; i++) {
     const random1000 = Math.floor(Math.random() * 1000)
     const randPrice = Math.floor(Math.random() * 20) + 10
+
     const camp = new Campground({
       author: '60b771403c217b4a08a84ebf',
       location: `${cities[random1000].city},${cities[random1000].state}`,
+      geometry: {
+        type: 'Point',
+        coordinates: [
+          cities[random1000].longitude,
+          cities[random1000].latitude,
+        ],
+      },
       images: [
         {
           url: 'https://res.cloudinary.com/duyd9br4i/image/upload/v1623000873/Scamp/tezcovrltrwxtqpd5hs8.jpg',
